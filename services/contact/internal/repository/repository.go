976db0be826e6c1f9cb9b2cedaf7c16b900cc098
mukash0124 <mukash.m0124@gmail.com>
2024-02-19 21:41:53 +0600

    some commit

---
+++ services/contact/internal/repository/repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import "architecture_go/services/contact/internal/domain"
+
+type ContactRepository interface {
+	CreateContact(contact domain.Contact) error
+	ReadContact(contactID int) (domain.Contact, error)
+	UpdateContact(contactID int, newContact domain.Contact) error
+	DeleteContact(contactID int) error
+
+	CreateGroup(group domain.Group) error
+	ReadGroup(groupID int) (domain.Group, error)
+
+	AddContactToGroup(contactID, groupID int) error
+}
